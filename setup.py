4e03422eb6ade2cdfd86223404873e23ebd41fb2
jakep-pdx <62121317+jakep-pdx@users.noreply.github.com>
2022-07-02 11:38:44 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+""" packaging config """
+import os
+from setuptools import setup, find_packages
+
+# with open("requirements.txt", encoding="utf-8") as req:
+#     requirements = req.read().splitlines()
+
+
+PKG_VERSION = "0.0.0"
+try:
+    new_ver = os.environ["new_ver"]
+    if new_ver:
+        PKG_VERSION = new_ver
+    else:
+        print("new_ver not found, using PKG_VERSION default:", PKG_VERSION)
+except KeyError:
+    print("new_ver not found, using PKG_VERSION default:", PKG_VERSION)
+
+setup(
+    name = "semantic_version_inator",
+    description = "Python packaging utility to generate semantic version in CI/CD workflow/pipeline",
+    version = PKG_VERSION,
+    python_requires = ">=3.10",
+    install_requires = "",
+    license="Apache License 2.0",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License'
+    ],
+    pakckages = find_packages(include=["semantic_version_inator", "semantic_version_inator.*"]),
+    entry_points = {
+        'console_scripts': [
+            'get_next_ver=semantic_version_inator.console:get_next_ver',
+            'get_next_ver_file_name=semantic_version_inator.console:get_next_ver_file_name'
+        ]
+    }
+)
